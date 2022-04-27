1692e75ae03cd6d9d98b5317108ebd2d28a4982c
d21emijo <d21emijo@student.his.se>
2022-04-27 15:00:07 +0200

    mountains + gson

---
+++ app/src/main/java/com/example/readjsonfromfiledemo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.readjsonfromfiledemo;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private final String TAG = "==>";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String json = readFile("mountains.json");
+        Log.d(TAG,json);
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<mountain>>() {}.getType();
+        List<mountain> listOfMountains = gson.fromJson(json, type);
+        Log.d(TAG,"Number of element " + listOfMountains.size() );
+        Log.d(TAG , "element 0 "+ listOfMountains.get(0).toString());
+
+
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not read file: " + fileName);
+            return null;
+        }
+    }
+}
